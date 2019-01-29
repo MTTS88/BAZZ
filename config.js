df9f8cd6f646731921723cd93f5e1feded58ba32
MTTS88 <47149532+MTTS88@users.noreply.github.com>
2019-01-29 16:23:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.TOKEN = "";
+exports.TOKEN = "NTM5NTY4Njg2MzgyNDQ4NjQx.DzEQmg.JKpRs15Vja6cSBbXHF7sQlIvUpA";
 exports.PREFIX = "!";
 exports.GOOGLE_API_KEY = "AIzaSyCcOHGZ5WMd162u1ptFcTn45WXn2U39loM";
 
